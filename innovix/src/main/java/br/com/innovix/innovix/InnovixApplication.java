e55aff29ab15eeaa24527ce5bd156eef0e175ebf
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2024-03-26 18:39:27 -0300

    java init

---
+++ innovix/src/main/java/br/com/innovix/innovix/InnovixApplication.java
@@ -0,0 +1,13 @@
+package br.com.innovix.innovix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovixApplication.class, args);
+	}
+
+}
